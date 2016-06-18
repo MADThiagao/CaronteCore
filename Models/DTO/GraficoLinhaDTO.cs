e031f6dfd9b55cc52f708ce65b3fdedb7d0ea5e4
MADThiagao <tsgomes@outlook.com>
2016-06-18 04:10:40 -0300

    adição de DTO para alimentação do grafico linha

---
+++ Models/DTO/GraficoLinhaDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CaronteCore.Models.DTO
+{
+    public class GraficoLinhaDTO
+    {
+        public int Dia { get; set; }
+        public decimal ValorAtual { get; set; }
+        public decimal ValorProjecao { get; set; }
+    }
+}
